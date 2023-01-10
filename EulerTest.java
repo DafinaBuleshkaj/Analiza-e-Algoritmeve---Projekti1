651f9bab7fd9c596614f301bb619eb093676ee08
DafinaBuleshkaj <45096869+DafinaBuleshkaj@users.noreply.github.com>
2023-01-10 21:34:53 +0100

    Add files via upload

---
+++ EulerTest.java
@@ -0,0 +1,10 @@
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+
+public final class EulerTest {
+	
+	
+	@Test public void testP425() { assertEquals("46479497324"  , new p425().run()); }
+	
+}
